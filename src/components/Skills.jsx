32b44889cac543f2537975e6af7747ab17d01dd4
Shashi <shashitiwari1997@gmail.com>
2021-04-23 16:22:53 +0530

    update with skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import "./css/skills.css";
+import html from "./resources/skills/html.svg";
+import figma from "./resources/skills/figma.svg";
+import excel from "./resources/skills/excel.svg";
+import javascript from "./resources/skills/javascript.svg";
+import mysq from "./resources/skills/mysq.svg";
+import cssIcon1 from "./resources/skills/cssIcon1.png";
 import { HashLink as Link } from "react-router-hash-link";
 
 function Skills() {
   return (
     <div className="skills" id="skill">
-      <h1> Hey greeting from skills Page</h1>
-
+      <h1> My Skills</h1>
+      <div className="achievement">
+        <p className="achievementTitle">Look What I can Do</p>
+        <div className="skill">
+          <img src={html} alt="html" />
+          <img src={cssIcon1} alt="cssIcon1" />
+          <img src={javascript} alt="javascript" />
+          <img src={figma} alt="figma" />
+          <img src={mysq} alt="mysq" />
+          <img src={excel} alt="excel" />
+        </div>
+      </div>
       <Link className="top" smooth to="#top">
         Top
       </Link>
